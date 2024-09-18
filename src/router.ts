00c559a4dcd59ba918963856963f9474a9d81ee9
huy <huyducphamofficial@gmail.com>
2024-09-18 17:49:11 +0700

    Feat: Implement Flowbite Typography, implement default blog page (https://flowbite.com/blocks/publisher/blog-templates/#default-blog-template)

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,16 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from './pages/home/home.vue';
+import Home from './pages/home/Home.vue';
+import PostDetail from "./pages/post-detail/PostDetail.vue";
 
 const routes: Array<any> = [
     {
         path: '/',
         component: Home,
+    },
+    {
+        path: '/post-detail', //Route paths should start with a "/": "post-detail" should be "/post-detail"
+        component: PostDetail,
+        name: 'post_detail',
     }
 ];
 const router = createRouter({
