d708254173b19d2def6a7484c08ab07cc96d484d
Huy Pham <huyducphamofficial@gmail.com>
2025-02-01 23:03:08 +0700

    Feat: PostIndex -> Sorting: Highlighting for currently selected option

--- src/pages/post-index/PostIndex.vue
+++ src/pages/post-index/PostIndex.vue
@@ -54,7 +54,8 @@ fetch();
             <input id="default-radio-1"
               class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               name="default-radio" type="radio" value="">
-            <label class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 text-nowrap select-none"
+            <label
+                class="ms-2 text-sm font-bold text-gray-900 dark:text-gray-300 text-nowrap select-none cursor-pointer"
               for="default-radio-1">Default
               radio</label>
           </div>
@@ -62,7 +63,8 @@ fetch();
             <input id="default-radio-2" checked
               class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               name="default-radio" type="radio" value="">
-            <label class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 text-nowrap select-none"
+            <label
+                class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 text-nowrap select-none cursor-pointer"
               for="default-radio-2">Checked
               state</label>
           </div>
