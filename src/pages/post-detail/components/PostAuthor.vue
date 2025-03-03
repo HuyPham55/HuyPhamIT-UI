ac42c5f3fa7241825d8fa48220d5255e483109a9
Kenny Pham <kenny@vitex.asia>
2025-03-03 19:14:34 +0700

    19 | Add wrapper to post content

---
+++ src/pages/post-detail/components/PostAuthor.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="not-format">
+    <address class="flex items-center mb-6 not-italic">
+      <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+        <img alt="Jese Leos" class="mr-4 w-16 h-16 rounded-full"
+             src="https://flowbite.com/docs/images/people/profile-picture-2.jpg">
+        <div>
+          <a class="text-xl font-bold text-gray-900 dark:text-white" href="#" rel="author">Jese Leos</a>
+          <p class="text-base text-gray-500 dark:text-gray-400">Graphic Designer, educator & CEO Flowbite</p>
+          <p class="text-base text-gray-500 dark:text-gray-400">
+            <time datetime="2022-02-08" pubdate title="February 8th, 2022">Feb. 8, 2022</time>
+          </p>
+        </div>
+      </div>
+    </address>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
