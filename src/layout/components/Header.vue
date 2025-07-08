4e126144020e915efb214dd8cfb4eb48b88788ef
Huy Pham <huyducphamofficial@gmail.com>
2025-07-08 19:15:03 +0700

    25 | Feat: Add LoginButton component and integrate into Header

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -6,6 +6,7 @@ import SearchInput from "@/layout/components/components/SearchInput.vue";
 import UserMenu from "@/layout/components/components/UserMenu.vue";
 import {initFlowbite} from "flowbite";
 import {useRouter} from "vue-router";
+import LoginButton from "@/layout/components/components/LoginButton.vue";
 
 /*
 Disable search button that toggles search input on mobile devices, even it's on mobile
@@ -61,12 +62,7 @@ onUpdated(() => {
         <SearchButton/>
         <SwitchThemeButton/>
 
-        <button v-if="!computedLoggedIn" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                @click="login"
-                type="button">
-          Log in
-        </button>
-
+        <LoginButton v-if="!computedLoggedIn" @click="login"/>
         <UserMenu v-if="computedLoggedIn"/>
 
         <button aria-controls="navbar-sticky" aria-expanded="false"
---
+++ src/layout/components/components/LoginButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {computed} from "vue";
+import {useRouter} from "vue-router";
+
+const isLoginPage = computed(() => router.currentRoute.value.name === 'login');
+const router = useRouter()
+
+</script>
+
+<template>
+  <button :class="['transition', isLoginPage ? 'text-blue-700 bg-transparent border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:text-blue-500 dark:border-blue-500 dark:hover:bg-blue-600 dark:hover:text-white dark:focus:ring-blue-800' : 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800']"
+          type="button">
+    Log in
+  </button>
+</template>
+
+<style scoped>
+
+</style>
